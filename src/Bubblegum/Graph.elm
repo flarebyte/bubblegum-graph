1ddcf2efef47b6a8e96c459cb6ad267f26453888
olih <olih@users.noreply.github.com>
2018-02-14 21:03:11 +0000

    Tests for creating nodes

--- src/Bubblegum/Graph.elm
+++ src/Bubblegum/Graph.elm
@@ -1,9 +1,9 @@
-module Bubblegum.Graph exposing(render)
+module Bubblegum.Graph exposing(createNode, createEdge, createGraph)
 
 {-| This library provides a directed graph model for representing relationships between UI components.
 
-# Selection
-@docs render
+# Construction of graph
+@docs createNode, createEdge, createGraph
 
 -}
 import List
@@ -11,96 +11,52 @@ import Set exposing (Set)
 import Maybe
 
 
-{-| Render.
+{-| The core representation of a value.
 -}
-render: String
-render = ""
+type alias EdgeModel eData= {
+    id: String
+    , source: String
+    , destination: String
+    , value: eData
+ }
+
+type alias NodeModel nData= {
+    id: String
+    , value: nData
+ }
 
 {-| The core representation of a value.
 -}
-type alias Model = {
-    fields: List FieldModel.Model
-    , edges: List FieldEdge.Model
-    , values: List FieldValue.Model
-    , states: Set String
+type alias GraphModel nData eData = {
+    nodes: List  (NodeModel nData)
+    , edges: List (EdgeModel eData)
+  }
+
+{-| Creates a node.
+-}
+createNode: String -> nData -> NodeModel nData
+createNode id nodeData=
+  {
+    id = id
+    , value = nodeData
  }
 
-matchFieldId: String -> FieldModel.Model -> Bool
-matchFieldId id field = field.id == id
+{-| Creates a edge.
+-}
+createEdge: String -> String -> String -> eData -> EdgeModel eData
+createEdge id source destination edgeData=
+  {
+    id = id
+    , source = source
+    , destination = destination
+    , value = edgeData
+ }
 
-findFieldModel: Model -> String -> Maybe FieldModel.Model
-findFieldModel model id =
-  model.fields |> List.filter (matchFieldId id)|> List.head
-
-matchEdgeId: String -> FieldEdge.Model -> Bool
-matchEdgeId id field = field.id == id
-
-findFieldEdge: Model -> String -> Maybe FieldEdge.Model
-findFieldEdge model id =
-  model.edges |> List.filter (matchEdgeId id)|> List.head
-
-matchEdgeSource: String -> FieldEdge.Model -> Bool
-matchEdgeSource id field = field.from == id
-
-findFieldEdgesBySource: Model -> String -> List FieldEdge.Model
-findFieldEdgesBySource model id =
-  model.edges |> List.filter (matchEdgeSource id)
-
-findDestinationsBySource: Model -> String -> List String
-findDestinationsBySource model id =
-  findFieldEdgesBySource model id |> List.map .to
-
-findDestinationsBySourceAsTuple: Model -> String -> (String, List String)
-findDestinationsBySourceAsTuple model id =
-  (id, (findDestinationsBySource model id))
-
-findFieldEdges: Model -> List String -> List (Maybe FieldEdge.Model)
-findFieldEdges model path =
-    List.map (findFieldEdge model) path
-
-matchPath: List String -> FieldValue.Model -> Bool
-matchPath path value =
-  value.path == path
-
-findFieldValue: Model -> List String -> Maybe FieldValue.Model
-findFieldValue model path =
-  model.values |> List.filter (matchPath path)|> List.head
-
-findSourceFieldModelByEdgeId: Model -> String -> Maybe FieldModel.Model
-findSourceFieldModelByEdgeId model edgeId =
-    findFieldEdge model edgeId |> Maybe.map .from |> Maybe.andThen (findFieldModel model)
-
-findDestFieldModelByEdgeId: Model -> String -> Maybe FieldModel.Model
-findDestFieldModelByEdgeId model edgeId =
-    findFieldEdge model edgeId |> Maybe.map .to |> Maybe.andThen (findFieldModel model)
-
-surelyHead: List String -> String
-surelyHead list = List.head list |> Maybe.withDefault "/?"
-
--- [b, a] -> return [e, b, a] and [f, b , a]
-expandDestinations: Model -> List String -> List (List String)
-expandDestinations model path =
-    surelyHead path |> findDestinationsBySource model |> List.map (\p -> p :: path)
-
-
-flattenList : List (List a) -> List a
-flattenList list =
-  List.foldr (++) [] list
-
-type alias TempPathStack = {
-    total:  List (List String)
-    , toExpand:  List (List String)
-}
--- algorithm:
--- child to parent is left to right
--- all processes the longest path
---  [a]
---  add [b, a] add [c, a]
---  add [e, b, a] add [f, b , a] add [g, c, a]
-findPathsFromSources: Model -> TempPathStack -> TempPathStack
-findPathsFromSources model stack  =
-    let
-         toExpandNext = stack.toExpand |>  List.map (expandDestinations model) |> List.concat      
-    in
-        { toExpand = toExpandNext , total = toExpandNext ++ stack.total }
- 
+{-| Render.
+-}
+createGraph: List  (NodeModel nData) -> List (EdgeModel eData) -> GraphModel nData eData
+createGraph nodes edges=
+  {
+    nodes = nodes
+    , edges = edges
+  }
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,8 +2,22 @@ module Tests exposing (..)
 
 import Test exposing (describe, test, Test)
 import Expect
-import Bubblegum.Graph exposing (render)
+import Bubblegum.Graph exposing (createNode, createEdge, createGraph)
+import List
+import Array
 
+asNode: Int -> String
+asNode num =
+    "node_" ++ toString(num)
+    
+nodes=List.range 0 10 |> List.map (\i -> createNode (asNode i) (asNode i))
+
+edge1_2 = createEdge "edge1_2" (asNode 1) (asNode 2) "edge1_2"
+edge2_3 = createEdge "edge2_3" (asNode 2) (asNode 3) "edge2_3"
+edge3_4 = createEdge "edge3_4" (asNode 3) (asNode 4) "edge3_4"
+edges = [edge1_2, edge2_3, edge3_4]
+
+myGraph = createGraph nodes edges
 
 all : Test
 all =
@@ -11,15 +25,6 @@ all =
         [ describe "createGraph" <|
             [ test "create a graph" <|
                 \() ->
-                    tripleEqual
-                    (WidgetModel.fromTriples subjectId myIncSpinner |> WidgetModel.toTriples)
-                    (myIncSpinner)               
-              
-           ,  test "create a medium-text" <|
-                \() ->
-                    tripleEqual
-                    (WidgetModel.fromTriples subjectId myMediumText |> WidgetModel.toTriples)
-                    (myMediumText)               
-              
+                    Expect.equal (List.length myGraph.edges) 3   
             ]
         ]
