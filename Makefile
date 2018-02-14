9fa62725f400ea706262a90bf62b5a9da219ce8f
olih <olih@users.noreply.github.com>
2018-02-14 19:52:12 +0000

    Adds starting point
    without .DS_store

---
+++ Makefile
@@ -0,0 +1,25 @@
+SRC = src
+BUILD = build
+
+reset:
+	rm -rf elm-stuff
+	rm -rf tests/elm-stuff
+	rm -rf build
+
+build: test build-directory js
+
+install:
+	elm-package install -y
+	pushd tests && elm-package install -y && popd
+
+build-directory:
+	mkdir -p $(BUILD)
+
+js:
+	elm-make src/Bubblegum/Graph.elm --output $(BUILD)/graph.js
+
+test:
+	cd tests && elm-test Main.elm
+
+doc:
+	elm-make --docs=documentation.json
---
+++ src/Bubblegum/Graph.elm
@@ -0,0 +1,106 @@
+module Bubblegum.Graph exposing(render)
+
+{-| This library provides a directed graph model for representing relationships between UI components.
+
+# Selection
+@docs render
+
+-}
+import List
+import Set exposing (Set)
+import Maybe
+
+
+{-| Render.
+-}
+render: String
+render = ""
+
+{-| The core representation of a value.
+-}
+type alias Model = {
+    fields: List FieldModel.Model
+    , edges: List FieldEdge.Model
+    , values: List FieldValue.Model
+    , states: Set String
+ }
+
+matchFieldId: String -> FieldModel.Model -> Bool
+matchFieldId id field = field.id == id
+
+findFieldModel: Model -> String -> Maybe FieldModel.Model
+findFieldModel model id =
+  model.fields |> List.filter (matchFieldId id)|> List.head
+
+matchEdgeId: String -> FieldEdge.Model -> Bool
+matchEdgeId id field = field.id == id
+
+findFieldEdge: Model -> String -> Maybe FieldEdge.Model
+findFieldEdge model id =
+  model.edges |> List.filter (matchEdgeId id)|> List.head
+
+matchEdgeSource: String -> FieldEdge.Model -> Bool
+matchEdgeSource id field = field.from == id
+
+findFieldEdgesBySource: Model -> String -> List FieldEdge.Model
+findFieldEdgesBySource model id =
+  model.edges |> List.filter (matchEdgeSource id)
+
+findDestinationsBySource: Model -> String -> List String
+findDestinationsBySource model id =
+  findFieldEdgesBySource model id |> List.map .to
+
+findDestinationsBySourceAsTuple: Model -> String -> (String, List String)
+findDestinationsBySourceAsTuple model id =
+  (id, (findDestinationsBySource model id))
+
+findFieldEdges: Model -> List String -> List (Maybe FieldEdge.Model)
+findFieldEdges model path =
+    List.map (findFieldEdge model) path
+
+matchPath: List String -> FieldValue.Model -> Bool
+matchPath path value =
+  value.path == path
+
+findFieldValue: Model -> List String -> Maybe FieldValue.Model
+findFieldValue model path =
+  model.values |> List.filter (matchPath path)|> List.head
+
+findSourceFieldModelByEdgeId: Model -> String -> Maybe FieldModel.Model
+findSourceFieldModelByEdgeId model edgeId =
+    findFieldEdge model edgeId |> Maybe.map .from |> Maybe.andThen (findFieldModel model)
+
+findDestFieldModelByEdgeId: Model -> String -> Maybe FieldModel.Model
+findDestFieldModelByEdgeId model edgeId =
+    findFieldEdge model edgeId |> Maybe.map .to |> Maybe.andThen (findFieldModel model)
+
+surelyHead: List String -> String
+surelyHead list = List.head list |> Maybe.withDefault "/?"
+
+-- [b, a] -> return [e, b, a] and [f, b , a]
+expandDestinations: Model -> List String -> List (List String)
+expandDestinations model path =
+    surelyHead path |> findDestinationsBySource model |> List.map (\p -> p :: path)
+
+
+flattenList : List (List a) -> List a
+flattenList list =
+  List.foldr (++) [] list
+
+type alias TempPathStack = {
+    total:  List (List String)
+    , toExpand:  List (List String)
+}
+-- algorithm:
+-- child to parent is left to right
+-- all processes the longest path
+--  [a]
+--  add [b, a] add [c, a]
+--  add [e, b, a] add [f, b , a] add [g, c, a]
+findPathsFromSources: Model -> TempPathStack -> TempPathStack
+findPathsFromSources model stack  =
+    let
+         toExpandNext = stack.toExpand |>  List.map (expandDestinations model) |> List.concat      
+    in
+        { toExpand = toExpandNext , total = toExpandNext ++ stack.total }
+ 
