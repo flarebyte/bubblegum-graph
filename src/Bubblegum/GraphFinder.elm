7dab16de6525677180a3248577ce63e30f9f35bd
olih <olih@users.noreply.github.com>
2018-02-25 15:12:34 +0000

    wip

--- src/Bubblegum/GraphFinder.elm
+++ src/Bubblegum/GraphFinder.elm
@@ -109,16 +109,33 @@ type alias GraphIndex = {
 
 noNode = {id= "!!!no-node!!!", value = NoNode}
 
+
+{-| Takes a key-fn and a list.
+  Creates a `Dict` which maps the key to a list of matching elements.
+    groupBy String.length [ "tree" , "apple" , "leaf" ]
+    --> Dict.fromList [ ( 4, [ "tree", "leaf" ] ), ( 5, [ "apple" ] ) ]
+  Code borrowed from: https://github.com/elm-community/dict-extra/blob/master/src/Dict/Extra.elm
+-}
+groupBy : (a -> comparable) -> List a -> Dict comparable (List a)
+groupBy keyfn list =
+    List.foldr
+        (\x acc ->
+            Dict.update (keyfn x) (Maybe.map ((::) x) >> Maybe.withDefault [ x ] >> Just) acc
+        )
+        Dict.empty
+        list
+
 toGraphIndex: Graph nData eData -> MajorNodes -> GraphIndex
 toGraphIndex graph majorNodes =
   let
     nodes = graph.nodes |> List.map (\n-> (n.id, {id = n.id, value = findNodeRole majorNodes n.id})) |> Dict.fromList
-    sourceEdges = graph.edges |> List.map (\e -> (e.source, {e | value = Irrelevant})) |> Dict.fromList
+    sourceEdges = graph.edges |> List.map (\e -> {e | value = Irrelevant}) |> groupBy .source
+    destEdges = graph.edges |> List.map (\e -> {e | value = Irrelevant}) |> groupBy .destination
   in
     {
       nodes = nodes
       , sourceEdges = sourceEdges
-      , destEdges = sourceEdges --TODO
+      , destEdges = destEdges
     }
 
 
@@ -133,13 +150,13 @@ findNodeMeta graph id =
 -}
 findSourceMeta: GraphIndex -> String -> List EdgeMeta
 findSourceMeta graph src =
-  Dict.get src graph.sourceEdge
+  Dict.get src graph.sourceEdges |> Maybe.withDefault []
 
 {-| find edge models by destination.
 -}
 findDestinationMeta: GraphIndex -> String -> List EdgeMeta
 findDestinationMeta graph dest =
-  Dict.get  dest graph.destEdges
+  Dict.get  dest graph.destEdges |> Maybe.withDefault []
 
 {-| find all the major nodes for the graph
 -}
